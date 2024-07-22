b25e8ac1b2864e50bdd13a01f83b9321746014c5
Legendile7 <diveshg9@gmail.com>
2024-07-22 18:17:26 -0400

    url update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://uga-scs-competitive-hacking.github.io',
+  url: 'https://github.com/UGA-SCS-Competitive-Hacking',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/main-website/',
