44b7cef6845b73588c1b27b416adc55a75bedf6d
Legendile7 <diveshg9@gmail.com>
2024-07-22 18:07:02 -0400

    baseurl update part 2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://uga-scs-competitive-hacking.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/main-website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
