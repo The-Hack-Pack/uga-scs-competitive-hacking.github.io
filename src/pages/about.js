2de4a4231911e2e8e196a6fd903d097bc7fc24db
Legendile7 <diveshg9@gmail.com>
2024-07-29 19:41:55 -0400

    fix images not displaying on prod

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,28 +16,28 @@ export default function About() {
           <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
             {/* First Row */}
             <div style={{ flex: '0 0 30%', margin: '10px' }}>
-              <img src="img/divesh.webp" alt="Divesh Gupta" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
+              <img src={require('@site/static/img/divesh.webp').default} alt="Divesh Gupta" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
               <p style={{ margin: '5px 0', fontSize: '1.3em' }}><a href="https://www.linkedin.com/in/divesh2027/" target="_blank"><strong>Divesh Gupta</strong></a></p>
               <p style={{ margin: '5px 0', fontSize: '0.9em' }}>President</p>
             </div>
             <div style={{ flex: '0 0 30%', margin: '10px' }}>
-              <img src="img/ethan.webp" alt="Ethan Mund" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
+              <img src={require('@site/static/img/ethan.webp').default} alt="Ethan Mund" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
               <p style={{ margin: '5px 0', fontSize: '1.3em' }}><a href="https://www.linkedin.com/in/ethan-mund227/" target="_blank"><strong>Ethan Mund</strong></a></p>
               <p style={{ margin: '5px 0', fontSize: '0.9em' }}>Vice President</p>
             </div>
             {/* Second Row */}
             <div style={{ flex: '0 0 30%', margin: '10px' }}>
-              <img src="img/cole.webp" alt="Cole Nangle" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
+              <img src={require('@site/static/img/cole.webp').default} alt="Cole Nangle" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
               <p style={{ margin: '5px 0', fontSize: '1.3em' }}><a href="https://www.linkedin.com/in/cole-nangle/" target="_blank"><strong>Cole Nangle</strong></a></p>
               <p style={{ margin: '5px 0', fontSize: '0.9em' }}>Training Coordinator</p>
             </div>
             <div style={{ flex: '0 0 30%', margin: '10px' }}>
-              <img src="img/aparna.webp" alt="Aparna Sajith" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
+              <img src={require('@site/static/img/aparna.webp').default} alt="Aparna Sajith" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
               <p style={{ margin: '5px 0', fontSize: '1.3em' }}><a href="https://www.linkedin.com/in/aparna-sajith-a90716206/" target="_blank"><strong>Aparna Sajith</strong></a></p>
               <p style={{ margin: '5px 0', fontSize: '0.9em' }}>Marketing Officer</p>
             </div>
             <div style={{ flex: '0 0 30%', margin: '10px' }}>
-              <img src="img/sriram.webp" alt="SriRam Surisetty" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
+              <img src={require('@site/static/img/sriram.webp').default} alt="SriRam Surisetty" style={{ width: '70%', height: 'auto', borderRadius: '20%' }} />
               <p style={{ margin: '5px 0', fontSize: '1.3em' }}><a href="https://www.linkedin.com/in/sriram-surisetty-389483290/" target="_blank"><strong>SriRam Surisetty</strong></a></p>
               <p style={{ margin: '5px 0', fontSize: '0.9em' }}>Competition Coordinator</p>
             </div>
